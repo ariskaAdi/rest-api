380435bc9e0c2cde4183c208667e5c237dc9187d
ariskaAdi <ariskaadiprasetyo@gmail.com>
2025-06-16 22:46:29 +0700

    setup jwt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, {
+  Application,
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+} from "express";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 const app: Application = express();
@@ -9,12 +16,47 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
+interface UserData {
+  id: string;
+  name: string;
+  email: string;
+  address: string;
+}
+
+interface ValidatedRequest extends Request {
+  userData: UserData;
+}
+
 const PORT: number | string = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
+const validatedToken: RequestHandler = (req, res, next): void => {
+  const validationReq = req as ValidatedRequest;
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(401).json({ message: "Token diperlukan" });
+    return; // berhenti eksekusi, tapi TIDAK return Response secara eksplisit
+  }
+
+  const token = authHeader.split(" ")[1];
+  const secret = process.env.JWT_SECRET!;
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    if (typeof decoded !== "string") {
+      validationReq.userData = decoded as UserData;
+      next(); // sukses -> lanjut ke route handler berikutnya
+    } else {
+      res.status(401).json({ message: "Token tidak valid" });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Token tidak valid" });
+  }
+};
 // Routes
 // membuat endpoint baru untuk register
 app.use("/register", async (req, res) => {
@@ -55,6 +97,19 @@ app.use("/login", async (req, res) => {
   if (user && typeof user.password === "string") {
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (isPasswordValid) {
+      const payload = {
+        id: user?.id,
+        name: user?.name,
+        email: user?.email,
+        address: user?.address,
+      };
+
+      const secret = process.env.JWT_SECRET!;
+      // tanda seru di akhir memastikan bahwa secret akan selalu ada dan membaca dari env
+      const expiresIn = 60 * 60 * 1;
+
+      const token = jwt.sign(payload, secret, { expiresIn: expiresIn });
+
       res.status(200).json({
         data: {
           id: user?.id,
@@ -62,6 +117,8 @@ app.use("/login", async (req, res) => {
           email: user?.email,
           address: user?.address,
         },
+        token: token,
+        message: `welcome back ${user.name}`,
       });
     } else {
       res.status(404).json({
@@ -72,7 +129,7 @@ app.use("/login", async (req, res) => {
 });
 
 // get a list of users
-app.get("/users", async (req, res) => {
+app.get("/users", validatedToken, async (req, res) => {
   const result = await prisma.user.findMany({
     select: {
       name: true,
